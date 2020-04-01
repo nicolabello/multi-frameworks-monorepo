f1834f0e7372ba180787110ac1295ca2e1d8da70
Nicola Bello <nicola.bello.3@gmail.com>
2020-04-01 21:28:15 +0100

    added styles lib

--- apps/angular/src/app/app.component.ts
+++ apps/angular/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { MDCCheckbox } from '@material/checkbox';
+
 
 @Component({
   selector: 'ft-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'Feature Toggles';
+
+  public ngAfterViewInit(): void {
+    const chk = new MDCCheckbox(document.querySelector('.mdc-checkbox'));
+  }
+
 }
