972dc8af15f16d940aeb35b045c0d275ab2af6be
Nicola Bello <nicola.bello.3@gmail.com>
2020-04-18 14:08:15 +0100

    completed react project

---
+++ apps/react/src/modules/material-components-web/MDCSwitch.tsx
@@ -0,0 +1,29 @@
+import { MDCSwitch as MDCSwitchImport } from '@nicolabello/material-components-web';
+import React from 'react';
+
+class MDCSwitch extends React.Component<any> {
+
+  private ref = React.createRef<HTMLDivElement>();
+  private instance: MDCSwitchImport | undefined;
+
+  public componentDidMount(): void {
+    if (this.ref.current) {
+      this.instance = MDCSwitchImport.attachTo(this.ref.current);
+    }
+  }
+
+  public componentWillUnmount(): void {
+    if (this.instance) {
+      this.instance.destroy();
+    }
+  }
+
+  public render(): React.ReactNode {
+    return (
+      <div ref={this.ref} {...this.props}>{this.props.children}</div>
+    );
+  }
+
+}
+
+export default MDCSwitch;
