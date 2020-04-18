31ac38112a9aa049a8f91d4628e0611e12711936
Nicola Bello <nicola.bello.3@gmail.com>
2020-04-18 14:08:01 +0100

    completed react project

--- apps/react/src/components/feature-form/FeatureForm.tsx
+++ apps/react/src/components/feature-form/FeatureForm.tsx
@@ -4,9 +4,22 @@ import { Feature, FeatureValue } from '../../../../express/src/models/feature';
 import MDCButton from '../../modules/material-components-web/MDCButton';
 import MDCSelect from '../../modules/material-components-web/MDCSelect';
 import MDCSelectHelperText from '../../modules/material-components-web/MDCSelectHelperText';
+import MDCSwitch from '../../modules/material-components-web/MDCSwitch';
 import MDCTextField from '../../modules/material-components-web/MDCTextField';
 import MDCTextFieldHelperText from '../../modules/material-components-web/MDCTextFieldHelperText';
 import './FeatureForm.scss';
+
+// TODO: this should be imported, but it's giving error "imports outside of src/ are not supported"
+enum FeatureValueType {
+  String = 'string',
+  Number = 'number',
+  Boolean = 'boolean',
+}
+
+// TODO: this should be imported, but it's giving error "imports outside of src/ are not supported"
+const featureValueTypes: string[] = Object.keys(FeatureValueType).map(
+  (key) => (FeatureValueType as any)[key] as string
+);
 
 function FeatureForm(props: { data?: Feature, onCancel: () => any, onSubmit: (values: Feature) => any }) {
 
@@ -32,8 +45,7 @@ function FeatureForm(props: { data?: Feature, onCancel: () => any, onSubmit: (va
 
       if (!values.type) {
         errors.type = 'This is required';
-        // TODO: this should use featureValueTypes,but it's giving error "imports outside of src/ are not supported"
-      } else if (!['string', 'number', 'boolean'].find(item => values.type === item)) {
+      } else if (!featureValueTypes.find(item => values.type === item)) {
         errors.type = 'Value not allowed';
       }
 
@@ -43,26 +55,26 @@ function FeatureForm(props: { data?: Feature, onCancel: () => any, onSubmit: (va
     onSubmit: values => props.onSubmit(values)
   });
 
-  const setType = (value: any) => {
+  const setType = (value: FeatureValueType) => {
     if (form.values.type !== value) {
       form.setFieldValue('type', value);
       updateValue(value);
     }
   };
 
-  const updateValue = (type: any) => {
+  const updateValue = (type: FeatureValueType) => {
     const value = form.values.value;
     let newValue: FeatureValue;
     switch (type) {
-      case 'boolean':
+      case FeatureValueType.Boolean:
         newValue = typeof value === 'boolean' ? value : value === 'true';
         newValue !== value && form.setFieldValue('value', newValue);
         break;
-      case 'number':
+      case FeatureValueType.Number:
         newValue = parseFloat(value as string) || null;
         newValue !== value && form.setFieldValue('value', newValue);
         break;
-      case 'string':
+      case FeatureValueType.String:
         newValue = typeof value !== 'undefined' && value !== null ? `${value}` : '';
         newValue !== value && form.setFieldValue('value', newValue);
         break;
@@ -114,7 +126,7 @@ function FeatureForm(props: { data?: Feature, onCancel: () => any, onSubmit: (va
 
       <MDCSelect className="mdc-select" required={true} invalid={form.touched.type && !!form.errors.type}
                  value={form.values.type}
-                 onChange={(value: any) => setType(value)}>
+                 onChange={(value: FeatureValueType) => setType(value)}>
         <div className="mdc-select__anchor">
           <span className="mdc-select__dropdown-icon"/>
           <div aria-controls="type-helper-text-id" aria-describedby="type-helper-text-id"
@@ -134,6 +146,62 @@ function FeatureForm(props: { data?: Feature, onCancel: () => any, onSubmit: (va
       </MDCSelect>
       <MDCSelectHelperText aria-hidden="true" className="mdc-select-helper-text mdc-select-helper-text--validation-msg"
                            id="type-helper-text-id">{form.errors.type}</MDCSelectHelperText>
+
+      {form.values.type === FeatureValueType.Boolean ?
+        <div className="ft-switch" role="presentation">
+          <MDCSwitch className="mdc-switch">
+            <div className="mdc-switch__track"/>
+            <div className="mdc-switch__thumb-underlay">
+              <div className="mdc-switch__thumb"/>
+              <input aria-checked={form.values.value === true} className="mdc-switch__native-control"
+                     id="value-boolean-id"
+                     role="switch" type="checkbox" name="value" defaultChecked={form.values.value === true}
+                     onChange={form.handleChange} onBlur={form.handleBlur}/>
+            </div>
+          </MDCSwitch>
+          <label htmlFor="value-boolean-id">Off/On</label>
+        </div>
+        : null}
+
+      {form.values.type === FeatureValueType.Number ?
+        <>
+          <MDCTextField className="mdc-text-field" invalid={form.touched.value && !!form.errors.value}
+                        value={form.values.value || form.values.value === 0 ? `${form.values.value}` : ''}>
+            <span className="mdc-text-field__ripple"/>
+            <input aria-controls="value-helper-text-id" aria-describedby="value-helper-text-id"
+                   aria-labelledby="value-id"
+                   className="mdc-text-field__input" type="number" name="value"
+                   value={form.values.value || form.values.value === 0 ? `${form.values.value}` : ''}
+                   onChange={form.handleChange} onBlur={form.handleBlur}/>
+            <span className="mdc-floating-label" id="value-id">Value</span>
+            <span className="mdc-line-ripple"/>
+          </MDCTextField>
+          <div aria-hidden="true" className="mdc-text-field-helper-line" id="value-helper-text-id">
+            <MDCTextFieldHelperText
+              className="mdc-text-field-helper-text mdc-text-field-helper-text--validation-msg">{form.errors.value}</MDCTextFieldHelperText>
+          </div>
+        </>
+        : null}
+
+      {form.values.type === FeatureValueType.String ?
+        <>
+          <MDCTextField className="mdc-text-field" invalid={form.touched.value && !!form.errors.value}
+                        value={form.values.value && form.values.value !== true ? `${form.values.value}` : ''}>
+            <span className="mdc-text-field__ripple"/>
+            <input aria-controls="value-helper-text-id" aria-describedby="value-helper-text-id"
+                   aria-labelledby="value-id"
+                   className="mdc-text-field__input" type="text" name="value"
+                   value={form.values.value && form.values.value !== true ? `${form.values.value}` : ''}
+                   onChange={form.handleChange} onBlur={form.handleBlur}/>
+            <span className="mdc-floating-label" id="value-id">Value</span>
+            <span className="mdc-line-ripple"/>
+          </MDCTextField>
+          <div aria-hidden="true" className="mdc-text-field-helper-line" id="value-helper-text-id">
+            <MDCTextFieldHelperText
+              className="mdc-text-field-helper-text mdc-text-field-helper-text--validation-msg">{form.errors.value}</MDCTextFieldHelperText>
+          </div>
+        </>
+        : null}
 
       <pre className="ft-feature-preview">{preview()}</pre>
 
